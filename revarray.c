3175eae816c46eb39b45dbfdcfde4bf092e25dba
Max Kezhaev <64354962+makezh@users.noreply.github.com>
2021-12-30 01:23:22 +0000

    add some new files

---
+++ revarray.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+
+void revarray(void *base, unsigned long nel, unsigned long width)
+{
+	void *tmp = malloc(width);
+	
+	for(int i = 0; i < nel/2; i++)
+	{
+		void *left = base + i * width, *right = base + (nel - 1 - i) * width;
+		
+		memcpy(tmp, right, width);
+		memcpy(right, left, width);
+		memcpy(left, tmp, width);
+	}
+	
+	free(tmp);
+}
+
+
+
+int main()
+{
+	/*char a[] = {1, 2, 3, 4, 5};
+	int size = 5;
+	revarray(a, size, sizeof(char));
+
+	for (int i = 0; i < size; i++) 
+		printf("%i ", a[i]);*/
+
+return 0;
+}
