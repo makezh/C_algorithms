77e7bc2e1bb7af44d735f29a8aed831073b8150c
Max Kezhaev <64354962+makezh@users.noreply.github.com>
2022-01-14 21:12:40 +0000

    fix dsort

--- dsort.c
+++ dsort.c
@@ -1,36 +1,35 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
+#include <stdlib.h>
+
+#define MAX_CHAR 26
+
+void key(char* str, int count[])
+{
+	int n = strlen(str);
+	for(int i = 0; i < n; i++)
+		count[str[i]-'a']++;
+}
 
 void dsort(char* str) {
-    int count[26] = {0};
-    char* k = str;
+		int count[26] = {0};
 
-    while (*k != '\0') 
-		{
-      count[*k - 'a']++;
-      k++;
-    }
+		key(str,count);
 
-    for (int i = 0; i < 26; i++)		
-		{
-      while (count[i]) 
-			{
-        *str = (char)i + 'a';
-				count[i]--;
-        str++;
-      }
-    }
+		for (int i = 0; i < MAX_CHAR; i++)
+			for(int j = 0; j < count[i]; j++)
+				printf("%c", (char)(i+'a'));
+
 }
 
 int main() {
-    char* str=NULL;
-		if(scanf("%m[^\n]", &str) == 1)
+		char* str=NULL;
+		if(scanf("%m[^\n]", &str))
 		{
 			dsort(str);
-			printf("%s\n", str);
 			free(str);
 		}
+
 		else printf("\n");
-    return 0;
+		return 0;
 }
